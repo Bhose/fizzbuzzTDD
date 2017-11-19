50cd65789200e664a1708b3dd99f089cae41aa76
Brittney Hose <bhose10@gmail.com>
2017-11-19 11:11:11 -0500

    Corrected mod placement

--- fizzbuzzTDD.rb
+++ fizzbuzzTDD.rb
@@ -1,11 +1,11 @@
 def number (test_number)
 	number = test_number
-	if number % 3 == 0
-		"fizz"
+	if number % 3 == 0 && number % 5 == 0
+		"fizzbuzz"
 	elsif number % 5 == 0 
 		"buzz"
-	elsif number % 3 == 0 && number % 5 == 0
-		"fizzbuzz"
+	elsif number % 3 == 0
+		"fizz"
 	end
 
 end
